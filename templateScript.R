3fde622937b7d65a5550ff36d022decc3a099c56
London <Mark.London@ext.ons.gov.uk>
2024-12-10 14:50:41 +0000

    script template improved

--- templateScript.R
+++ templateScript.R
@@ -1,6 +1,7 @@
-#' @title hello_world
-#' @description  A happy greeting
+#' @title hello
+#' @description A happy greeting
 #' @author Mark London \email{mark.london@ext.ons.gov.uk}
 #' @details Some more details
-#' @example hello_world()
+#' @examples
+#'  hello()
 #' @export
